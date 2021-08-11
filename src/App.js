63e7efa0135b2dc2dc3c69c3bdfd298876eb8459
Andrew Rogers <adrewrog@amazon.com>
2021-08-11 11:38:52 -0500

    Coomplete info card modal

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   Navbar,
   Container,
-  Nav,
-  NavDropdown
+  Nav
 } from "react-bootstrap";
 
 function App() {
@@ -32,13 +31,6 @@ function App() {
             <Nav className="me-auto">
               <Nav.Link href="/about">About</Nav.Link>
               <Nav.Link href="/users">Users</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/views/home/pokemon/PokemonInfo.js
+++ src/views/home/pokemon/PokemonInfo.js
@@ -3,17 +3,36 @@ import PropTypes from 'prop-types';
 import Modal from 'react-bootstrap/Modal';
   
   // Info modal 
-  const PokemonInfo = ({ name, show, handleClose }) => (
+  const PokemonInfo = ({ name, base, show, handleClose }) => (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>{name.english}</Modal.Title>
       </Modal.Header>
+      <Modal.Body>
+          <h5>Properties:</h5>
+          <ul>
+            <li>HP: {base.HP}</li>
+            <li>Attack: {base.Attack}</li>
+            <li>Defense: {base.Defense}</li>
+            <li>Special Attack: {base.SpAttack}</li>
+            <li>Special Defense: {base.SpDefense}</li>
+            <li>Speed: {base.Speed}</li>
+          </ul>          
+        </Modal.Body>
     </Modal>
   );
   
   PokemonInfo.propTypes = {
     name: PropTypes.shape({
       english: PropTypes.string,
+    }),
+    base: PropTypes.shape({
+      HP: PropTypes.number,
+      Attack: PropTypes.number,
+      Defense: PropTypes.number,
+      SpAttack: PropTypes.number,
+      SpDefense: PropTypes.number,
+      Speed: PropTypes.number
     })
   };
 
